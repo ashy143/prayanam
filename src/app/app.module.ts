07c9ae0c872c0ccc0a7bea118da11788436fff0b
ashy143 <ash.patthi@gmail.com>
2018-09-11 00:40:24 -0400

    adding search component and material design module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AppRoutingModule } from './routes/app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+//import { MaterialComponentsModuleModule } from './material-components-module/material-components-module.module'
+
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/layout/header/header.component';
@@ -12,6 +17,7 @@ import { ContactComponent } from './home/contact/contact.component';
 import { FeaturesComponent } from './home/features/features.component';
 import { PopularComponent } from './home/popular/popular.component';
 import { CardComponent } from './home/popular/card/card.component';
+import { SearchComponent } from './home/search/search.component';
 
 @NgModule({
   declarations: [
@@ -19,18 +25,25 @@ import { CardComponent } from './home/popular/card/card.component';
     HeaderComponent,
     FooterComponent,
     HomeComponent,
-    HomeComponent,
     ContactComponent,
     FeaturesComponent,
     PopularComponent,
-    CardComponent
+    CardComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
     MDBBootstrapModule.forRoot(),
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
+//    MaterialComponentsModuleModule
+    
   ],
   providers: [SearchService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }      
+export class AppModule { }
+
